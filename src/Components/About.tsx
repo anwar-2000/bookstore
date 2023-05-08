290371f629ef57b26b53641b30dbe40a81063e01
anwar-2000 <khaledkhaouani5@gmail.com>
2023-05-08 19:56:51 +0100

    fixed responsivness

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const SectionContainer = styled.section`
   padding: 20px;
   margin-top : 5rem;
-  margin-left : 3rem;
+
   h1{
       font-size: 2.9rem;
       font-weight: 400;
@@ -15,7 +15,7 @@ const SectionContainer = styled.section`
 const Header = styled.h1`
   text-align: center;
   margin-bottom: 20px;
-  transform : translateX(-2rem);
+  
 
   @media (min-width: 768px) {
     font-size : 10px;
@@ -30,11 +30,13 @@ const ColumnsContainer = styled.div`
   gap: 20px;
   @media (min-width: 768px) {
     flex-direction: row;
+    margin-left : 2rem;
   }
 `;
 
 const TextColumn = styled.div`
   flex: 1;
+  text-align : center;
 `;
 
 const ImageColumn = styled.div`
@@ -44,6 +46,10 @@ const ImageColumn = styled.div`
 const Image = styled.img`
   width: 100%;
   mix-blend-mode : multiply;
+  @media (max-width: 768px) {
+    
+    margin-left : 1rem;
+  }
 `;
 
 
@@ -53,14 +59,14 @@ const  imageUrl = "./logo.jpg"
       <SectionContainer>
         <Header>À propos de nous</Header>
         <ColumnsContainer>
-          <TextColumn>A l’origine : En 1949 l’abbé Pierre est appelé au chevet d’un ancien bagnard. N’ayant rien à lui
+          <TextColumn>A l&apos;origine : En 1949 l&apos;abbé Pierre est appelé au chevet d&apos;un ancien bagnard. N&apos;ayant rien à lui
 donner, il lui propose :
-« Puisque tu veux mourir, tu n’as rien à perdre. Alors donne-moi ton aide pour aider les autres. »
-Cet homme, Georges, devient le premier de tous les compagnons d’Emmaüs.
-L’association Emmaüs est créée en 1953 pour organiser et développer ce mouvement. Après les
-ravages de la guerre de1939-45, les rigueurs de l’hiver 1954 tuent. Dans ce contexte de graves
-pénuries de logements, l’abbé Pierre lance son célèbre appel, « Mes amis, au secours ! » ; il
-déclenche l’insurrection de la Bonté » et influence fortement les pouvoirs publics. <br />
+« Puisque tu veux mourir, tu n&apos;as rien à perdre. Alors donne-moi ton aide pour aider les autres. »
+Cet homme, Georges, devient le premier de tous les compagnons d&apos;Emmaüs.
+L&apos;association Emmaüs est créée en 1953 pour organiser et développer ce mouvement. Après les
+ravages de la guerre de1939-45, les rigueurs de l&apos;hiver 1954 tuent. Dans ce contexte de graves
+pénuries de logements, l&apos;abbé Pierre lance son célèbre appel, « Mes amis, au secours ! » ; il
+déclenche l&apos;insurrection de la Bonté » et influence fortement les pouvoirs publics. <br />
     <Link href={'/emmausNaintre'} style={{color : 'blue'}}>Voir Plus ...</Link>
 </TextColumn>
           <ImageColumn>
