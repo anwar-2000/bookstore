5462c5d9efe3a505c39722702c8cb0a5ac9e76eb
anwar-2000 <khaledkhaouani5@gmail.com>
2023-06-20 21:30:38 +0100

    SEO URL SHARING

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,12 @@ export default function Home() {
   <Head>
   <title>EMMAUS NAINTRE</title>
   <link rel="icon" href="./logo.jpg" />
+  <meta property="og:image" content="https://emmtaboutique.com/logo.jpg"></meta>
+  <meta property="twitter:image" content="https://emmtaboutique.com/logo.jpg"></meta>
+  <meta property="twitter:card" content="Emmaüs Naintré - Chatellerault "></meta>
+  <meta property="twitter:title" content="La Boutique enligne Emmaüs Naintré"></meta>
+  <meta property="twitter:description" content="La boutique des livres Emmaus Naintré propose des livres rares, anciens, des bandes dessinées (BD) et des livres de poche à des prix compétitifs. Découvrez notre large sélection de livres français et élargissez votre bibliothèque avec nos trouvailles uniques."></meta>
+  <meta property="og:url" content="https://emmtaboutique.com"></meta>
   <meta name="description" content="La boutique des livres Emmaus Naintré propose des livres rares, anciens, des bandes dessinées (BD) et des livres de poche à des prix compétitifs. Découvrez notre large sélection de livres français et élargissez votre bibliothèque avec nos trouvailles uniques."  />
   <meta name="keywords" content="Livres Rares, Livres Anciens, Bandes Dessinées, BD, Livres de Poche, Livres Français, Emmaus Naintré, Boutique Chatellerault" />
   <meta name="author" content="Emmaus Naintré - Chatellerault" />
