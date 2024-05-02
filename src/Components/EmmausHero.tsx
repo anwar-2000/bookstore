5534b510846b5283763954995256940bed30ba64
anwar-2000 <khaledkhaouani5@gmail.com>
2024-05-02 12:15:20 +0200

    added small anim to hero , the time i continue editing designs

--- src/Components/EmmausHero.tsx
+++ src/Components/EmmausHero.tsx
@@ -34,15 +34,22 @@ const EmmausHero = () => {
               height={600}
             />
     </ImagesContainer>
-    <Text
-      initial ={{opacity : 0}}
+    <Text>
+        <motion.h1
+         initial ={{opacity : 0}}
       whileInView={{opacity : 1}}
-      transition={{delay : 1 , duration : 0.4}}
-    >
-        <h1>Bienvenue chez</h1>
-        <h1>Emmaüs Châtellerault</h1>
-        <small> votre destination pour des découvertes uniques !</small>
-        <Link href="/all"><button>Explorer la boutique</button></Link>
+      transition={{delay : 0.8 , duration : 0.4}}>Emmaüs Châtellerault</motion.h1>
+        <motion.small 
+        initial ={{opacity : 0}}
+      whileInView={{opacity : 1}}
+      transition={{delay : 1 , duration : 0.4}}>Notre boutique en ligne vous permet de dénicher des objets farfelus, rares, ou plus courants mais tout aussi intéressants, comme si vous étiez dans l'un des rayons d'un de nos 4 lieux de vente.
+
+Vous y trouverez des livres anciens mais aussi des plus récents, des objets vintage mais pas que, des vêtements pour toutes et tous, des chaussures, de la maroquinerie, mais aussi de quoi intéresser les plus jeunes avec des jeux et livres jeunesse.</motion.small>
+        <Link href="/all"><motion.button
+         initial ={{opacity : 0 , y : -10 , background : "none"}}
+      whileInView={{opacity : 1 , y :0 , background : "white" }}
+      transition={{delay : 1.2 , duration : 0.8}}
+      >Explorer la boutique</motion.button></Link>
     </Text>
   </Container>
 }
@@ -57,6 +64,17 @@ const Container = styled.div`
     flex-direction:column;
     align-items : flex-start;
     justify-content : flex-start;
+    &::before {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(
+            to bottom,
+            rgba(0, 0, 0, 0.6),
+            rgba(0, 0, 0, 0.8)
+        );
+    }
     
 `
 const ImagesContainer = styled.div`
@@ -64,13 +82,7 @@ const ImagesContainer = styled.div`
     width : 100%;
     height : 100%;
     z-index : -1;
-    &::before {
-        content: "";
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.6); 
-    }
+   
     img {
         width : 100%;
         height : 100%;   
@@ -88,15 +100,17 @@ const Text = styled(motion.div)`
    z-index:3;
 
    h1{
-     font-size : 4rem;
+     font-size : 7rem;
      color : white;
      font-weight:bolder;
    }
    small{
-    font-size : 1.5rem;
+    font-size : 1.2rem;
     font-family: "Libre Baskerville", serif;
     font-weight: 400;
+    width : 50%;
     font-style: italic;
+    text-align : center;
     color:white;
    }
    button{
@@ -113,6 +127,7 @@ const Text = styled(motion.div)`
     }
     small{
       font-size : 0.9rem;
+      width : 80%;
     }
     button{
       padding : 0.4rem 2rem;
